f79d9f879aa208245c4f97800ece814f47338f19
MitchCay <caywoodmitch@gmail.com>
2020-11-20 18:18:36 -0600

    uploading the glassdoor scrapper and data collection code

---
+++ data_collection.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  4 18:24:55 2020
+
+@author: caywo
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+path = "C:/Users/caywo/Documents/ds_physics_related_proj"
+
+df = gs.get_jobs('physics', 1500, False, path+"/chromedriver", 15)
+
+df.to_csv(path+"/glassdoor_jobs.csv", index=False, header=True)
